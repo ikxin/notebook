9af6965cea82d6b7be76813e29bfbc09cb2d26dc
一纸忘忧 <i@ikxin.com>
2022-04-02 16:18:11 +0800

    背景移动

--- Canvas/js/background.js
+++ Canvas/js/background.js
@@ -12,3 +12,29 @@ function loadBackground(bgUrl, canvas, completeCallback) {
     }
     image.src = bgUrl
 }
+
+var backgroundOffset = 0
+
+// 实现背景滚动
+function animateBackground(canvas, image, speed) {
+    var context = canvas.getContext('2d')
+    var width = canvas.width
+    var height = canvas.height
+    // 清除画布
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    // 存储状态
+    context.save()
+    // 更新位置
+    backgroundOffset += speed
+
+    if (backgroundOffset >= height) {
+        backgroundOffset = 0
+    }
+
+    context.translate(0, backgroundOffset)
+    // 绘制图片
+    context.drawImage(image, 0, 0, width, height)
+    context.drawImage(image, 0, -height, width, height)
+    // 获取存储状态
+    context.restore()
+}
