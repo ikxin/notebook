65c8e0bbf6349737e7b83e199144ecd494baedbe
一纸忘忧 <i@ikxin.com>
2022-04-08 15:32:50 +0800

    画子弹

---
+++ Canvas/js/bullet.js
@@ -0,0 +1,24 @@
+function createBullet(bWidth, bHeight, bUrl, hero) {
+    var x = hero.x + hero.width / 4 - bWidth / 8
+    var y = hero.y - bHeight / 2 - 4
+    var bullet = new Bullet(x, y, bWidth, bHeight, bUrl)
+
+    // 子弹声音
+    var bSound = new Audio('../music/bullet.mp3')
+    // bSound.play()
+    return bullet
+}
+
+function Bullet(x, y, w, h, imageUrl) {
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+    this.image = new Image()
+    this.image.src = imageUrl
+}
+
+Bullet.prototype.draw = function (canvas) {
+    var context = canvas.getContext('2d')
+    context.drawImage(this.image, this.x, this.y, this.w / 2, this.h / 2)
+}
