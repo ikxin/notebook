1f2a37df9732e806c2911dc205958e7de3188945
一纸忘忧 <i@ikxin.com>
2022-05-09 14:25:44 +0800

    分析脚手架结构

---
+++ Vue/20_vue-cli/src/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 组件的结构代码 -->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学生地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+// 组件的交互代码
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "清华大学",
+      address: "中国北京",
+    };
+  },
+};
+</script>
+
+<style>
+/* 组件的样式代码 */
+.demo {
+  background-color: orange;
+}
+</style>
