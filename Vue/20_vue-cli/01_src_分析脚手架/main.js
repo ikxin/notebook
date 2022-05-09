70f6fd36f8d51e2bea2eccc1785760b0163c2838
一纸忘忧 <i@ikxin.com>
2022-05-09 16:54:56 +0800

    分析脚手架

---
+++ Vue/20_vue-cli/01_src_分析脚手架/main.js
@@ -0,0 +1,14 @@
+// 引入Vue
+import Vue from 'vue'
+// 引入App组件，它是所有组件的父组件
+import App from './App.vue'
+// 阻止 vue 在启动时生成生产提示
+Vue.config.productionTip = false
+
+// 创建Vue实例对象
+new Vue({
+  render: h => h(App),
+  // render(createElement){
+  //   return createElement('div','Hello World')
+  // }
+}).$mount('#app')
