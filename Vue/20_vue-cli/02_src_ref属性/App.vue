f06f70f763f81299341a918c9fc950c7e9010f24
一纸忘忧 <i@ikxin.com>
2022-05-09 16:56:27 +0800

    ref属性

---
+++ Vue/20_vue-cli/02_src_ref属性/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 v-text="msg" ref="title"></h2>
+    <button @click="showDOM">点击输出DOM</button>
+    <School ref="school" />
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "欢迎学习Vue",
+    };
+  },
+  components: {
+    School,
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.title); // 真实DOM元素
+      console.log(this.$refs.school); // School组件的实例对象
+    },
+  },
+};
+</script>
---
+++ Vue/20_vue-cli/02_src_ref属性/components/School.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学生地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "清华大学",
+      address: "中国北京",
+    };
+  },
+};
+</script>
