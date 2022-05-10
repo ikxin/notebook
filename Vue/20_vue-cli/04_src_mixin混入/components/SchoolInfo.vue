33849c6cd7fd3a339df82402615d16b6d0b4e81e
一纸忘忧 <i@ikxin.com>
2022-05-10 14:04:59 +0800

    mixin混入

---
+++ Vue/20_vue-cli/04_src_mixin混入/components/SchoolInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 @click="showName">学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+import { mixin, data } from '../mixin'
+export default {
+  name: 'SchoolInfo',
+  data () {
+    return {
+      name: '清华大学',
+      address: '中国北京'
+    }
+  },
+  mixins: [mixin, data],
+  mounted () {
+    console.log('world')
+  }
+}
+</script>
