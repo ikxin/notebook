775ea40c2af999230516c4ac02adf9c8a82bea7c
一纸忘忧 <i@ikxin.com>
2022-05-10 15:14:27 +0800

    scoped样式

---
+++ Vue/20_vue-cli/06_src_scoped样式/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SchoolInfo />
+    <hr />
+    <StudentInfo />
+  </div>
+</template>
+
+<script>
+import SchoolInfo from './components/SchoolInfo.vue'
+import StudentInfo from './components/StudentInfo.vue'
+export default {
+  name: 'App',
+  components: { SchoolInfo, StudentInfo }
+}
+</script>
