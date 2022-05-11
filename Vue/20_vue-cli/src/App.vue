1e99519e1fee54f7185561252951ddd37db2b351
一纸忘忧 <i@ikxin.com>
2022-05-11 23:15:23 +0800

    自定义事件

--- Vue/20_vue-cli/src/App.vue
+++ Vue/20_vue-cli/src/App.vue
@@ -1,118 +1,45 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <TodoAdd :addTodo="addTodo" />
-        <TodoList
-          :todos="todos"
-          :checkTodo="checkTodo"
-          :deleteTodo="deleteTodo"
-        />
-        <TodoCount
-          :todos="todos"
-          :checkAllTodo="checkAllTodo"
-          :clearAllTodo="clearAllTodo"
-        />
-      </div>
-    </div>
+  <div>
+    <h2>{{ name }}</h2>
+    <SchoolInfo :getSchoolName="getSchoolName" />
+    <hr />
+    <StudentInfo v-on:test="getStudentName" />
+    <!-- 只执行一次 -->
+    <!-- <StudentInfo v-on:test.once="getStudentName" /> -->
+    <hr />
+    <!-- 绑定原生DOM事件需要加native修饰符 -->
+    <StudentInfo ref="student" @click.native="showInfo" />
   </div>
 </template>
 
 <script>
-import TodoAdd from './components/TodoAdd.vue'
-import TodoList from './components/TodoList.vue'
-import TodoCount from './components/TodoCount.vue'
+import SchoolInfo from './components/SchoolInfo.vue'
+import StudentInfo from './components/StudentInfo.vue'
 export default {
   name: 'App',
-  components: { TodoAdd, TodoList, TodoCount },
+  components: { SchoolInfo, StudentInfo },
   data () {
     return {
-      todos: JSON.parse(localStorage.getItem('todos')) || []
+      name: ''
     }
   },
   methods: {
-    addTodo (todo) {
-      this.todos.unshift(todo)
+    getSchoolName (name) {
+      console.log('App收到了学校名称', name)
+      this.name = name
     },
-    checkTodo (id) {
-      this.todos.forEach((todo) => {
-        if (todo.id === id) {
-          todo.done = !todo.done
-        }
-      })
+    getStudentName (name) {
+      console.log('App收到了学生名称', name)
+      this.name = name
     },
-    deleteTodo (id) {
-      this.todos = this.todos.filter((todo) => {
-        return todo.id !== id
-      })
-    },
-    checkAllTodo (done) {
-      this.todos.forEach((todo) => {
-        todo.done = done
-      })
-    },
-    clearAllTodo () {
-      this.todos = this.todos.filter((todo) => {
-        return !todo.done
-      })
+    showInfo () {
+      console.log('点击了原生DOM事件')
     }
   },
-  watch: {
-    // todos (value) {
-    //   localStorage.setItem('todos', JSON.stringify(value))
-    // }
-    todos: {
-      deep: true,
-      handler (value) {
-        localStorage.setItem('todos', JSON.stringify(value))
-      }
-    }
+  mounted () {
+    this.$refs.student.$on('test', this.getStudentName)
+    // 只执行一次
+    // this.$refs.student.$once('test', this.getStudentName)
   }
 }
 </script>
-
-<style>
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
-    0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-</style>
