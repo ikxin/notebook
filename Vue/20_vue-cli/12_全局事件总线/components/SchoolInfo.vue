78e35f98f5fef5917d8a39fdf1254baaa54dfdac
一纸忘忧 <i@ikxin.com>
2022-05-12 17:23:32 +0800

    全局事件总线

---
+++ Vue/20_vue-cli/12_全局事件总线/components/SchoolInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SchoolInfo',
+  data () {
+    return {
+      name: '清华大学',
+      address: '中国北京'
+    }
+  },
+  mounted () {
+    this.$bus.$on('hello', (data) => {
+      console.log(data)
+    })
+  },
+  beforeDestroy () {
+    this.$bus.$off('hello')
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.demo {
+  background-color: pink;
+  padding: 10px;
+}
+</style>
