628f71cee9c2e77850890ffcf6dd3342f1de1613
一纸忘忧 <i@ikxin.com>
2022-05-14 22:58:51 +0800

    过度效果

--- Vue/20_vue-cli/src/components/Test.vue
+++ Vue/20_vue-cli/src/components/Test.vue
@@ -21,7 +21,8 @@ export default {
 h2 {
   background-color: pink;
 }
-.hello-enter-active {
+
+/* .hello-enter-active {
   animation: identifier 0.5s linear;
 }
 .hello-leave-active {
@@ -34,5 +35,19 @@ h2 {
   to {
     transform: translateX(0px);
   }
+} */
+.hello-enter-active,
+.hello-leave-active {
+  transition: 0.5s linear;
+}
+/* 进去的起点/离开的终点 */
+.hello-enter,
+.hello-leave-to {
+  transform: translateX(-100%);
+}
+/* 进入的终点/离开的起点 */
+.hello-enter-to,
+.hello-leave {
+  transform: translateX(0);
 }
 </style>
