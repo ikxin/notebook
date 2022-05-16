c4078f00f0cc1a573d4207588db8f70851f82d8e
一纸忘忧 <i@ikxin.com>
2022-05-16 23:10:30 +0800

    配置代理

--- Vue/20_vue-cli/vue.config.js
+++ Vue/20_vue-cli/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // devServer: {
+  //   proxy: 'http://localhost:5000'
+  // }
+  devServer: {
+    proxy: {
+      '/api': {
+        // 代理路径配置
+        target: 'http://localhost:5000',
+        // 用于重写路径
+        pathRewrite: { '^/api': '' },
+        // 用于支持websocket
+        ws: true,
+        // 用于控制请求头的host
+        changeOrigin: false
+      }
+    }
+  }
 })
